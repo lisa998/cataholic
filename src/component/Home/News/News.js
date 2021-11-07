b94d3e3203138d04f739a84f16c0cf9fbbd6924d
lisa <xj3zjfan@gmail.com>
2021-11-07 15:09:50 +0800

    fix news

--- src/component/Home/News/News.js
+++ src/component/Home/News/News.js
@@ -1,37 +1,24 @@
 import * as React from "react";
-import axios from "axios";
 import { Body, LinedDiv, Div, Content, Text, Img, Arrow } from "./styled";
 
 export default function News() {
   const [hover, setHover] = React.useState(0);
   const [hoverContent, setHoverContent] = React.useState(0);
-  const [result, setResult] = React.useState({
-    url: "",
-    urlToImage: "",
-    publishedAt: "",
-    author: "",
-    title: "",
-    description: "",
-  });
-  React.useEffect(() => {
-    (async function () {
-      let R = await axios.get(
-        "https://newsapi.org/v2/everything?qInTitle=cat&language=en&sortBy=popularity&pageSize=2&apiKey=08b7d6af7ef84eaaaf41343c06b5cd2a"
-      );
-      setResult({
-        url: R.data.articles[1].url,
-        urlToImage: R.data.articles[1].urlToImage,
-        publishedAt: R.data.articles[1].publishedAt,
-        author: R.data.articles[1].author,
-        title: R.data.articles[1].title,
-        description: R.data.articles[1].description,
-      });
-    })();
-  }, []);
+  const result = {
+    url: "https://www.bbc.com/news/uk-wales-59124053",
+    urlToImage:
+      "https://ichef.bbci.co.uk/news/976/cpsprodpb/780A/production/_121303703_newproject.png",
+    publishedAt: "Posted at 23:30 1 Nov23:30 1 Nov",
+    author: "BBC NEWS",
+    title: "Cardiff kitten saved from being set alight by children",
+    description:
+      "A kitten is recovering after he was rescued from three children threatening to set him alight.",
+  };
+
   return (
     <Body>
       <LinedDiv style={{ justifyContent: "flex-start" }}>popular news</LinedDiv>
-      <a href={result.url} style={{ textDecoration: "none" }}>
+      <a href={result.url} style={{ textDecoration: "none", color: "inherit" }}>
         <Content
           onMouseOver={() => setHoverContent(1)}
           onMouseLeave={() => setHoverContent(0)}
@@ -56,33 +43,38 @@ export default function News() {
           ></Arrow>
         </Content>
       </a>
-      <LinedDiv
-        onMouseOver={() => setHover(1)}
-        onMouseLeave={() => setHover(0)}
+      <a
+        href={"https://www.bbc.co.uk/search?q=cat&page=1"}
+        style={{ textDecoration: "none", color: "inherit" }}
       >
-        <div
-          hover={hover}
-          style={{
-            cursor: "pointer",
-            display: "flex",
-          }}
+        <LinedDiv
+          onMouseOver={() => setHover(1)}
+          onMouseLeave={() => setHover(0)}
         >
-          <Div hover={hover} show delay={0.05}>
-            {" "}
-            ⟶{" "}
-          </Div>
-          <Div hover={hover} show>
-            ⟶{" "}
-          </Div>
-          <Div hover={hover} style={{ opacity: 1 }}>
-            Discover all articles
-          </Div>
-          <Div hover={hover} delay={0.05}>
-            ⟶{" "}
-          </Div>
-          <Div hover={hover}> ⟶ </Div>
-        </div>
-      </LinedDiv>
+          <div
+            hover={hover}
+            style={{
+              cursor: "pointer",
+              display: "flex",
+            }}
+          >
+            <Div hover={hover} show delay={0.05}>
+              {" "}
+              ⟶{" "}
+            </Div>
+            <Div hover={hover} show>
+              ⟶{" "}
+            </Div>
+            <Div hover={hover} style={{ opacity: 1 }}>
+              Discover all articles
+            </Div>
+            <Div hover={hover} delay={0.05}>
+              ⟶{" "}
+            </Div>
+            <Div hover={hover}> ⟶ </Div>
+          </div>
+        </LinedDiv>
+      </a>
     </Body>
   );
 }
